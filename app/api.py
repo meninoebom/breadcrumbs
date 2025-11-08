afdfd789a26966bec56a1b26e4cea5504d7eb927
Brandon Brown <afrobot@gmail.com>
2025-11-07 19:02:10 -0800

    Remove malformed comments from api.py

--- app/api.py
+++ app/api.py
@@ -8,16 +8,4 @@ def read_root():
     return {"Hello": "World"}
 
 
-//GET crumbs get crumbs
-
-//GET crumbs/{id} get crumb
-
-//DELETE crumbs/{id} delete crumb
-
-//GET tags get all tags
-
-//GET tags/{id} get a tag
-
-//DELETE tags/{id} delete a tag
-
 
